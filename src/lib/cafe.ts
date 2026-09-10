4dec70c1f87ec4b6000b4a227be4ee46e557025c
Rahanpayao25 <rayhanpyo2016@gmail.com>
2026-09-10 19:41:02 +0800

    Update cafe.ts

--- src/lib/cafe.ts
+++ src/lib/cafe.ts
@@ -1,7 +1,7 @@
 export const CAFE = {
   name: "commune.",
   tagline: "WE COMMUNE, OVER COFFEE",
-  hours: "10:30AM - 12:30PM",
+  hours: "10:30AM - 11:30PM",
   hoursNote: "DAILY",
   street: "Crispin Atilano St., Tetuan",
   landmark: "near RF Mariano & San Lorenzo entrance",
